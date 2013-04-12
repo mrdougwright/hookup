eafb8d6605f06a8da9bec77ba102f9bc60462d79
Doug Wright <mrdougwright@gmail.com>
2013-04-11 17:44:21 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hookup::Application.config.secret_token = 'efbe967e83b061c1da1d0e07fd22a7d29e328058af456211099d69f04d703117b3ac7eab5e1ef9429825a9ce922c360c93cc28242579a321989537b8bcdbd797'
